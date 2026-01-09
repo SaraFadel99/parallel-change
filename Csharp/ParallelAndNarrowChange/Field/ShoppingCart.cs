1af2c2f7e5ea24b34b7843927dbf84fb0af629ad
Sara Fadel <sarafadel@clubbertv.com>
2026-01-09 21:33:34 +0200

    add support to list of items

--- Csharp/ParallelAndNarrowChange/Field/ShoppingCart.cs
+++ Csharp/ParallelAndNarrowChange/Field/ShoppingCart.cs
@@ -1,22 +1,48 @@
-﻿namespace ParallelAndNarrowChange.Field
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ParallelAndNarrowChange.Field
 {
-    public class ShoppingCart{
+    public class ShoppingCart
+    
+    {
         private decimal price;
+        private  List<int> prices = new List<int>();
 
         public decimal CalculateTotalPrice(){
             return price;
+        }
+
+        public decimal CalculateTotalPrices()
+        {
+            return prices.Sum();
         }
 
         public bool HasDiscount(){
             return price > 100;
         }
 
-        public void Add(int aPrice){
+        public bool HasDiscount(List<decimal> prices)
+        {
+            bool checkForDiscount = prices.Sum()>100;
+            return checkForDiscount;
+        }
+        public void Add(int aPrice)
+        {
             this.price = aPrice;
+        }
+        public void Add(List<int> prices)
+        {
+            this.prices = prices;
         }
 
         public int NumberOfProducts(){
             return 1;
+        }
+
+        public int NumberOfProductsItems()
+        {
+            return prices.Count();
         }
     }
 }
